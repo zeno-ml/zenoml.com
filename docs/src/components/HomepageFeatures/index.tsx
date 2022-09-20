f054f9a28293aaf6b92a575c7ad12aa8a01f8b46
erica-w-fu <efu@andrew.cmu.edu>
2022-09-20 17:03:15 -0400

    added main structure of website, placeholders for videos still need to be added

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 
+
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  video: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: "for any datatype",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    video: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Zeno's extensible <span className="heroEmphasis">Python API</span> can be used to run models, distill new metadata
@@ -21,7 +22,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "with no coding experience",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    video: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
         Zeno's interactive <span className="heroEmphasis">Exploration UI</span> can be used to explore model performance across a
@@ -31,7 +32,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "across models over time",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    video: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
         Zeno’s automated <span className="heroEmphasis">Analysis UI</span> tracks model performance discover dataset shift and regressions. It
@@ -42,25 +43,26 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, video, description }: FeatureItem) {
   return (
     <div className="row heroFeature">
-        <h1 className="heroSubtitle">Conduct behavioral evaluation on your models <span className="underline">{title}</span></h1>
-        <p>{description}</p>
-        <Svg className={styles.featureSvg} role="img" />
+        <div className="heroDescription">
+          <h1 className="heroSubtitle">Conduct behavioral evaluation <strong><em>{title}</em></strong></h1>
+          <p>{description}</p>
+        </div>
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+        <video className="heroDemo"></video>
     </div>
   );
 }
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
-      <div className="container">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        {/* </div> */}
-      </div>
-    </section>
+    <div className="container">
+        {FeatureList.map((props, idx) => (
+          <Feature key={idx} {...props} />
+        ))}
+      {/* </div> */}
+    </div>
   );
 }
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -11,7 +11,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className="heroBanner">
-      <h1 className="heroTitle">Conduct <span className="underline">behavioral evaluation</span> on your ML models
+      <h1 className="heroTitle">Conduct <strong><em>behavioral evaluation</em></strong> on your ML models
       </h1>
 
       <div className={styles.buttons}>
@@ -33,16 +33,36 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
+  const [show, setShow] = useState(true)
+  const controlIntroDemo = () => {
+    if (window.scrollY>500) {
+      setShow(false)
+    } else {
+      setShow(true)
+    }
+  }
+
+  useEffect (() => {
+    window.addEventListener('scroll', controlIntroDemo)
+  }, [])
+
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout
       title={`Zeno ML`}
       description="Machine learning evaluation framework"
     >
-      <HomepageHeader />
-      <main className="heroMain">
-        <HomepageFeatures />
-      </main>
+    <HomepageHeader />
+    <div className="heroDemoIntro">
+      <video className="heroDemo">
+        <source src="movie.mp4" className={`hideVid ${show && 'showVid'}`} type="video/mp4"></source>
+      </video>
+    </div>
+
+    <main className="heroMain">
+      <HomepageFeatures />
+    </main>
     </Layout>
   );
 }
