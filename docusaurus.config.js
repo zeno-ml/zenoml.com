bf0711835d327e0a90cfda1a985fc73f44519823
Alex Cabrera <alex.cabrera@gmail.com>
2023-02-01 08:18:26 -0500

    twitter preview card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "img/zeno.png",
       navbar: {
         // title: "Zeno",
         logo: {
