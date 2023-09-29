00b941da10eb07c84d9791c939659c808a8ae3db
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-29 12:48:24 -0400

    fix video

--- src/components/Demo/Demo.tsx
+++ src/components/Demo/Demo.tsx
@@ -4,9 +4,9 @@ import styles from "./index.module.css";
 export function Demo() {
   return (
     <div className={styles.demo}>
-      <video controls width="100%">
+      <video controls width="100%" autoPlay>
         <source
-          src={require("@site/static/img/teaser.mp4").default}
+          src={require("@site/static/img/teaser.m4v").default}
           type="video/mp4"
         ></source>
       </video>
