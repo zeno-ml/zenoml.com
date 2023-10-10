61402893b61150384478effac012585abe4b25ff
Alex Bäuerle <alex@a13x.io>
2023-10-10 17:59:41 +0200

    docs: add code example for rag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -160,7 +160,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["toml"],
+        additionalLanguages: ["toml", "json"],
       },
     }),
 };
