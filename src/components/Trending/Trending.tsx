67dce720345dabef57e03a3b784226a9a59941e3
Alex Bäuerle <alex@a13x.io>
2023-11-13 15:44:49 +0100

    feat: add trending projects and reports to homepage

---
+++ src/components/Trending/Trending.tsx
@@ -0,0 +1,22 @@
+import { Header } from "@site/src/pages";
+import React from "react";
+import styles from "./styles.module.css";
+
+export function Trending(props) {
+  return (
+    <div className={styles.container}>
+      <Header
+        title="Trending"
+        summary="Trending"
+        description="Explore trending reports and projects on Zeno."
+      ></Header>
+      <div className={styles.iframe}>
+        <iframe
+          height="250"
+          width="100%"
+          src="https://hub.zenoml.com/embed/entry/featured/4"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
